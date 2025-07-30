10188335db59322582287213cb46d382b196a71a
paulutsch <unoutsch@gmail.com>
2025-07-30 15:36:37 +0200

    feat: complete scoring for SudokuGame

--- sudokugame/master.py
+++ sudokugame/master.py
@@ -10,7 +10,7 @@ from clemcore.clemgame import (
     Observation,
     Player,
 )
-from clemcore.clemgame.metrics import BENCH_SCORE
+from clemcore.clemgame.metrics import BENCH_SCORE, METRIC_ABORTED, METRIC_SUCCESS
 from clemcore.utils.logger import format_json, setup_logger
 
 from sudokugame.game_environment import SudokuAction, SudokuEnvironment, SudokuPlayer
@@ -92,7 +92,7 @@ class SudokuGame(EnvGameMaster):
         }
         return action
 
-    def compute_response_score(self, response: str, context: Dict) -> float:
+    def compute_turn_score(self, response: str, context: Dict) -> float:
         """
         Compute a score for the player's response based on the environment state.
 
@@ -104,11 +104,11 @@ class SudokuGame(EnvGameMaster):
             float: The score for the response
         """
         logger.debug(
-            f"[_compute_response_score] Computing response score for response: {response}"
+            f"[_compute_turn_score] Computing response score for response: {response}"
         )
 
         score = 1.0 if self.game_environment.state["success"] else 0.0
-        logger.debug(f"[_compute_response_score] Response score: {score}")
+        logger.debug(f"[_compute_turn_score] Response score: {score}")
         return score
 
     def compute_episode_score(self) -> float:
@@ -127,39 +127,26 @@ class SudokuGame(EnvGameMaster):
 
 
 class SudokuGameScorer(GameScorer):
-    """
-    Scorer for the Sudoku Game.
-    Computes scores based on the game environment state.
-    """
+    """Scorer for the Sudoku game."""
 
     def __init__(self, game_name: str, experiment: Dict, game_instance: Dict):
         super().__init__(game_name, experiment, game_instance)
 
-    def compute_scores(self, episode_interactions: Dict) -> None:
-        """
-        Compute scores for the episode based on the interactions.
-        The Hello Game is scored based on whether the greeting was successful.
+    def compute_episode_scores(self, interactions: Dict) -> None:
+        """Compute episode-level scores for the Sudoku game.
 
         Args:
-            episode_interactions: Dictionary containing the episode interactions
+            interactions: Dict containing the logged episode's interactions.
         """
-        logger.debug(
-            f"Computing scores for episode interactions: \n{format_json(episode_interactions)}"
-        )
+        aborted = interactions.get(METRIC_ABORTED, False)
+        success = interactions.get(METRIC_SUCCESS, False)
 
-        success = 1 if episode_interactions["success"] else 0
-        aborted = 1 if episode_interactions["aborted"] else 0
-        episode_score = episode_interactions["episode_score"]
+        if aborted:
+            bench_score = 0.0
+        else:
+            bench_score = 100.0 if success else 0.0
 
-        self.log_episode_score("Success", success)
-        self.log_episode_score("Aborted", aborted)
-        self.log_episode_score("Episode Score", episode_score)
-
-        # bench score based on following instructions (not aborted) and winning (success)
-        not_aborted = 1 if not aborted else 0
-        bench_score = (not_aborted + success) / 2
         self.log_episode_score(BENCH_SCORE, bench_score)
-        logger.info(f"Final bench score: {bench_score}")
 
 
 class SudokuGameBenchmark(GameBenchmark):
